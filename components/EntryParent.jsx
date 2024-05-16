eebd51a3583ffd8e5cd14f4933e8cf4bdb204c80
Bradley A <138368512+boldybread@users.noreply.github.com>
2024-05-16 19:57:06 +1000

    feat: readonly entry component

---
+++ components/EntryParent.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Entry from "./Entry";
+
+export default function EntryParent(props){
+
+	let [editMode, setEditMode] = useState(false);
+
+	return(
+		<section>
+			{editMode ? <EntryForm entryData={props.entryData} /> : <Entry entryData={props.entryData} /> }
+
+			<button onClick={() => setEditMode(!editMode)}>Toggle Edit Mode</button>
+		</section>
+	)
+}
