c10b0b5cf83bf751ac1381aa098c6d873e18380e
Bradley A <138368512+boldybread@users.noreply.github.com>
2024-05-20 08:31:15 +1000

    feat: latest post redirect working

--- components/EntryForm.jsx
+++ components/EntryForm.jsx
@@ -7,24 +7,41 @@
 */
 
 import { useState } from "react"
+import { useJournalDispatch } from "../contexts/BlogContext";
+import { useNavigate } from "react-router-dom";
 
 export default function EntryForm(props){
 
-	let [localTitle, setLocalTitle] = useState(props.entryData.title);
-	let [localAuthor, setLocalAuthor] = useState(props.entryData.author);
-	let [localContent, setLocalContent] = useState(props.entryData.content);
+	let [localTitle, setLocalTitle] = useState((props.entryData && props.entryData.title) || "Default Title");
+	let [localAuthor, setLocalAuthor] = useState(props.entryData ? props.entryData.author : "Default Author");
+	let [localContent, setLocalContent] = useState(props.entryData?.content || "Hello world! Write a journal entry!");
 	// may do date later!
+
+
+	let addEntry = useJournalDispatch();
+
+	let navigate = useNavigate();
+
+	const submitEntry = () => {
+		addEntry(localTitle, localAuthor, localContent, Date.now(), props.entryData?.id);
+		navigate("/latest");
+	}
+
 
 	return(
 		<div>
 			<label htmlFor="entryTitle">Title:</label>
-			<input type="text" name="entryTitle" class="entryTitle" value={localTitle} onChange={(event) => setLocalTitle(event.target.value) }      />
+			<input type="text" name="entryTitle" className="entryTitle" value={localTitle} onChange={(event) => setLocalTitle(event.target.value) }      />
 
 			<label htmlFor="entryAuthor">Author:</label>
-			<input type="text" name="entryAuthor" class="entryAuthor" value={localAuthor} onChange={(event) => setLocalAuthor(event.target.value) }      />
+			<input type="text" name="entryAuthor" className="entryAuthor" value={localAuthor} onChange={(event) => setLocalAuthor(event.target.value) }      />
 
 			<label htmlFor="entryContent">Content:</label>
-			<input type="text" name="entryContent" class="entryContent" value={localContent} onChange={(event) => setLocalContent(event.target.value) }      />
+			<input type="text" name="entryContent" className="entryContent" value={localContent} onChange={(event) => setLocalContent(event.target.value) }      />
+
+			<button onClick={submitEntry}>
+				Submit entry
+			</button>
 		</div>
 	)
 }
--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { useLocalStorage } from 'react-use';
+import EntryParent from './components/EntryParent';
+import EntryForm from './components/EntryForm';
+import { useJournalData } from './contexts/BlogContext';
 
 function App() {
 
-  let [journalEntries, setJournalEntries] = useState([]);
-  let [storedEntries, setStoredEntries] = useLocalStorage("journalEntries");
+  // let someExampleGlobalState = useJournalData();
 
-  useEffect(() => {
-    setJournalEntries(storedEntries);
-
-    return (() => {
-      setStoredEntries(journalEntries);
-    });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  let journalEntries = useJournalData();
 
   return (
     <div className="App">
-      {journalEntries.length > 0 && <p>{JSON.stringify(journalEntries)}</p>}
+
+      {/* {someExampleGlobalState} */}
+
+      {/* {journalEntries?.length > 0 && <p>{JSON.stringify(journalEntries)}</p>} */}
+
+      <EntryForm entryData={{}} /> 
+
+      {journalEntries?.map((entry, index) => {
+        return <EntryParent key={index} entryData={entry}  />
+      })}
     </div>
   );
 }
